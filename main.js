0ff531b23daca2728be7208b3a6a75bbee843ac2
caiobas <caiobas6@hotmail.com>
2020-07-21 14:15:49 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
+// REST
+// Objeto REST
 const usuario = {
   nome: 'Caio',
   idade: 21,
-  endereco: {
-    cidade: 'São Paulo',
-    estado: 'SP',
-  },
+  empresa: 'Rocketseat'
 };
 
-function mostraNome({ nome, idade }){
-  console.log(nome, idade);
+const { nome, ...resto} = usuario;
+
+console.log(nome);
+console.log(resto);
+
+// Array REST
+const arr = [1, 2, 3, 4];
+
+const [ a, b, ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Function REST
+function soma(a, b, ...params) {
+  return params;
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4, 5, 6, 7, 8));
+
+// SPREAD
+//Array SPREAD
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+//Objeto SPREAD
+const usuario1 = {
+  nome: 'Caio',
+  idade: 21,
+  empresa: 'Rocketseat'
+};
+
+const usuario2 = { ...usuario1, nome: 'Gabriel'};
+
+console.log(usuario2);
