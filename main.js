29940c57e8cf9574936f6db5611a882895399c02
caiobas <caiobas6@hotmail.com>
2020-07-21 14:19:18 -0300

    Template literals

--- main.js
+++ main.js
@@ -1,48 +1,5 @@
-// REST
-// Objeto REST
-const usuario = {
-  nome: 'Caio',
-  idade: 21,
-  empresa: 'Rocketseat'
-};
+const nome = "Caio";
+const idade = 21;
 
-const { nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-// Array REST
-const arr = [1, 2, 3, 4];
-
-const [ a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-//Function REST
-function soma(a, b, ...params) {
-  return params;
-}
-
-console.log(soma(1, 3, 4, 5, 6, 7, 8));
-
-// SPREAD
-//Array SPREAD
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-//Objeto SPREAD
-const usuario1 = {
-  nome: 'Caio',
-  idade: 21,
-  empresa: 'Rocketseat'
-};
-
-const usuario2 = { ...usuario1, nome: 'Gabriel'};
-
-console.log(usuario2);
+// console.log('Meu nome é ' + nome + ' e tenho '+ idade + ' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
