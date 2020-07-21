397b50065e7f80581ad991b4f743957d46c1f0f8
caiobas <caiobas6@hotmail.com>
2020-07-21 14:53:36 -0300

    Configurando Webpack

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const nome = 'Caio';
-const idade = 21;
+import { soma } from './funcoes';
 
-const usuario = {
-  nome,
-  idade,
-  empresa: 'Rocketseat'
-};
-
-console.log(usuario);
+console.log(soma(1, 2));
