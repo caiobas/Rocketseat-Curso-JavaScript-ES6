21ed3ba957d0ff062bf6b634aba9675f107058b8
caiobas <caiobas6@hotmail.com>
2020-08-22 04:51:51 -0300

    Async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-alert('Testando');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// await só executado dentro de uma função async
+// async function executaPromise() {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+executaPromise();
